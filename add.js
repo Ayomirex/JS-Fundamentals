3ef09f34d607dd1c8ac4de7ea5c861e15df8e4e0
Ayomirex <Animashaunmistura21@gmail.com>
2025-05-25 15:42:19 -0700

    updated ninth one

--- add.js
+++ add.js
@@ -1,8 +1,8 @@
-function factorial(n) {
-    if (isNaN(n) || n < 0) return 1;
-    if (n === 0 || n === 1) return 1;
-    return n * factorial(n - 1);
+function add(a, b) {
+    return a + b;
   }
   
-  const num = parseInt(process.argv[2]);
-  console.log(factorial(num));
+  const first = parseInt(process.argv[2]);
+  const second = parseInt(process.argv[3]);
+  
+  console.log(add(first, second));
